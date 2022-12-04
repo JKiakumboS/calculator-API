c0103a7f55518b7ecb60f4043a296ac4a14b304f
João Kiakumbo Sebastião <jkiakumbo@joaos-macbook-pro.home>
2022-12-04 15:32:47 +0100

    first commit

---
+++ src/test/java/me/jkiakumbo/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jkiakumbo.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
